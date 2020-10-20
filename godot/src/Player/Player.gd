ec67a505141e3d7814b5fa8a2a3e2cd60e0c8316
KAN <kilian.anchyse@gmail.com>
2020-10-21 01:15:53 +0200

    First commit

---
+++ godot/src/Player/Player.gd
@@ -0,0 +1,9 @@
+tool
+class_name Player
+extends KinematicBody
+# Helper class for the Player scene's scripts to be able to have access to the
+# camera and its orientation.
+
+onready var camera: CameraRig = $CameraRig
+onready var skin: Mannequiny = $Soink
+onready var state_machine: StateMachine = $StateMachine
